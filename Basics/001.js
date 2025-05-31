3453169c08643535127c3b81d1c786955e835a0a
Aryan <aryan.rastogi.20nov@gmail.com>
2025-05-31 22:11:49 +0000

    conversion datatypes

--- Basics/001.js
+++
@@ -1,7 +0,0 @@
-const accountId = 1000101
-let accountEmail = "aryan@gmail.com"
-var accountPass = "121121"
-
-accountCity = "lucknow"
-
-console.table([accountId,accountEmail,accountPass,accountCity])
---
+++ Basics/002_conversion.js
@@ -0,0 +1,36 @@
+"use strict";
+
+let score ="33abc"
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let scoreNumber = Number(score);
+// console.log(scoreNumber)
+// console.log(typeof scoreNumber);
+
+let score2=null
+let scoreNumber2= Number(score2)
+// console.log(scoreNumber2)
+// console.log(typeof scoreNumber2)
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1
+
+let isloggedIn = ""
+let booleanisloggedIn = Boolean(isloggedIn)
+// console.log(booleanisloggedIn)
+// console.log(typeof booleanisloggedIn);
+
+// "0" => true
+//"" => false
+// 1 => true
+
+let number = 33
+let stringNumber = String(number);
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
+

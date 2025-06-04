ae34916242384bd06100a0fec3b9d6609fcac2ee
Aryan <aryan.rastogi.20nov@gmail.com>
2025-06-04 18:28:09 +0000

    Output dala hai

--- Basics/003_str_conv.js
+++ Basics/003_str_conv.js
@@ -6,7 +6,7 @@ let str3 = str1+str2
 
 console.log("1" +2); //12
 console.log(1 +"2"); //12
-console.log("1+2");  
+console.log("1+2");  // 1+2
 console.log("1"+"2"); //12
 console.log("1"+2+2); //122
 console.log(1+2+"2"); //32

f261ecb9ff6c235026df6895e1b350b282ffdd59
Aryan <aryan.rastogi.20nov@gmail.com>
2025-06-05 20:53:19 +0000

    modified subah dekhunga bc

--- Basics_pt2/004_objects.js
+++ Basics_pt2/004_objects.js
@@ -1,2 +1,24 @@
 //Object Constructors
 
+// const tinderUser= new Object();
+const tinderUser = {}
+
+// console.log(tinderUser);
+
+tinderUser.id = "123abc";
+tinderUser.name = "John Doe";
+tinderUser.isActive = true;
+
+const RegualrUser = {
+    email: "some@google.com",
+    fullname : {
+        userfullname: {
+            firstName: "John",
+            lastName: "Doe" 
+        
+        }
+    }
+}
+console.log(RegualrUser.fullname.userfullname.firstName);
+
+const obj1

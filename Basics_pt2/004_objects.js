055c32ea2668d9bb24732fb92e2540b97b7ed2d3
Aryan <aryan.rastogi.20nov@gmail.com>
2025-06-06 11:18:06 +0000

    object part 2 ( join objects)

--- Basics_pt2/004_objects.js
+++ Basics_pt2/004_objects.js
@@ -21,5 +21,21 @@ const RegualrUser = {
 }
 console.log(RegualrUser.fullname.userfullname.firstName);
 
-const obj1 ={1: "a", 2: "b", 3: "c"};
+const obj1 ={1: "a", 2: "b"};
+const obj2 ={3: "a", 4: "b"};
 
+// const obj3 = {obj1, obj2};   #1
+// const obj3 = Object.assign({}, obj1, obj2);  #2
+//{} is optional Target,Souce
+
+const obj3 = {...obj1, ...obj2};   //#3
+
+
+console.log(obj3);
+
+
+
+console.log(Object.keys(tinderUser)); // returns an array of keys
+console.log(Object.values(tinderUser)); // returns an array of values
+
+console.log(Object.entries(tinderUser)); // returns an array of key-value pairs

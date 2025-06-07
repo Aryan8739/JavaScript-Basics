0f655e4339fe46d8cf587f75feb70f9b6fac34d0
Aryan <aryan.rastogi.20nov@gmail.com>
2025-06-07 07:44:48 +0000

    Destructor nd JSON

---
+++ Basics_pt2/005_objects.js
@@ -0,0 +1,25 @@
+//Destructuring Objects
+
+
+const course = {
+    coursename : "JavaScript",
+    price : 1000,
+    teacher : "aryan"
+}
+
+const {teacher: tea}= course;
+console.log(tea); // Output: aryan
+
+//JSON 
+
+// {
+//     "name": "Aryan",
+//     "Coursename": "JavaScript",
+//     "price": 1000
+// }
+
+// [
+//     {},
+//     {},
+//     {}
+// ]

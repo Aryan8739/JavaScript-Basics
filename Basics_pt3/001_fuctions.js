3cceb5e8352753889fdf39a7fd6839ee9e11fec3
Aryan <aryan.rastogi.20nov@gmail.com>
2025-06-07 11:04:26 +0000

    Fuction

--- Basics_pt3/001_fuctions.js
+++ Basics_pt3/001_fuctions.js
@@ -4,6 +4,23 @@ function Saymyname(){
 
 // Saymyname()
 
-function addTwoNum(numbe1,number2){
-    cons
+// function addTwoNum(number1,number2){
+//     console.log(number1 + number2);
+// }
+
+function addTwoNum(number1,number2){
+    // let result =  console.log(number1 + number2);
+    // return result 
+    return number1 + number2;
+    // console.log("Aryan")  
 }
+
+let result = addTwoNum(10,20);
+// console.log("result:",result);
+
+function loginUser(username){
+    return `${username} Just Logged in`
+}
+
+console.log(loginUser("Aryan"));
+

7e04fb8737394db7f90efbaa95ce985c37cf6310
Aryan <aryan.rastogi.20nov@gmail.com>
2025-06-08 08:16:38 +0000

    if else

---
+++ Control flow/one.js
@@ -0,0 +1,30 @@
+//if
+
+// if(true) {
+//     console.log("This will always run");
+// } else {
+//     console.log("This will never run");
+// }
+
+// < , > , <= , >= , == , != , ===
+
+const score = 100;
+if (score > 90) {
+    let power = "fly"
+    console.log(`your power is ${power}`);
+}
+
+
+const balance = 1000;
+if (balance > 500) console.log("You are rich"),
+console.log("money");                            // Implicit return
+
+// never do like thiss
+
+
+
+
+
+
+
+

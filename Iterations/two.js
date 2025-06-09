6ac06945a81a05d70188489a80bafbbb1ec6d2af
Aryan <aryan.rastogi.20nov@gmail.com>
2025-06-09 15:31:44 +0530

    Add files via upload

---
+++ Iterations/two.js
@@ -0,0 +1,19 @@
+let i=0
+while (2==2) {
+    console.log(`Value of index ${i}`)
+    i=i+2;
+}
+
+let myArray = ['flash', 'Batman' , 'Superman']
+
+let arr=0
+while (arr<myArray.length()){
+    console.log(`Value is ${myArray[arr]}`);
+    arr=arr+1;
+}
+
+let h =1
+do {
+    console.log(`score is ${score}`);
+    score++
+} while (score<=10);
